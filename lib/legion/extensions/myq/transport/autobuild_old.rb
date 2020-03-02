5e6aa0c3bfd084e5d37b1619330c508b61185066
miverso2 <matt.iverson@optum.com>
2020-03-02 08:10:57 -0600

    Fixing transport autobuild

--- lib/legion/extensions/myq/transport/autobuild_old.rb
+++ lib/legion/extensions/myq/transport/autobuild_old.rb
@@ -1,10 +1,10 @@
 require 'legion/transport/exchanges/task'
-require 'legion/extensions/transport/autobuild'
+require 'legion/extensions/transport'
 
 module Legion::Extensions::Myq
   module Transport
     module AutoBuild
-      extend Legion::Extensions::Transport::AutoBuild
+      extend Legion::Extensions::Transport
     end
   end
 end
